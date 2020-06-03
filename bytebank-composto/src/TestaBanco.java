fddc6bdd2d3582695e1b198693a43260bffcaafd
r-fsantos <renato@renato>
2020-06-03 18:14:43 -0300

    Refazer aula 04

---
+++ bytebank-composto/src/TestaBanco.java
@@ -0,0 +1,19 @@
+public class TestaBanco {
+	public static void main(String[] args) {
+	    Cliente renato = new Cliente();
+
+	    renato.nome = "Renato Felício";
+	    renato.cpf = "045.567.598-97";
+	    renato.profissao = "Estudante";
+
+	    Conta contaDoRenato = new Conta();
+	    contaDoRenato.deposita(100);
+
+	    System.out.println("Saldo da conta do Renato R$ "+contaDoRenato.saldo+" Reais");
+
+	    // duas classes ate o momento -> como associar/referenciar os objetos de dois tipos diferentes?
+		// composicao de objetos
+		contaDoRenato.titular = renato;
+		System.out.println(contaDoRenato.titular.nome);
+    }
+}
