6647e36004c9a384bd5591f39ea8d64536713094
r-fsantos <renato@renato>
2020-06-10 15:26:02 -0300

    Formatando código.

--- bytebank-composto/src/Conta.java
+++ bytebank-composto/src/Conta.java
@@ -5,51 +5,11 @@ public class Conta {
     // intanciar -> criar objeto, instância de uma classe de tal TIPO
     //              manipulavel, como se existisse fisicamente
     //              implementacao do meu modelo Conta
-    //private static int quantidadeDeContas;
-    //private
     int agencia;
-    //private
     int conta;
-    //private
     int numero; // para que mesmo?
-    //private
     double saldo;
-    //private
     Cliente titular;
-
-    // construtor
-    //public Conta(int agencia, int numero) { // rotina de inicializacao -> n eh um metodo
-      //  quantidadeDeContas++;
-       // System.out.println("O total de contas no banco bytebank é de: " + quantidadeDeContas);
-        //this.agencia = agencia;
-        //this.numero = numero;
-        //System.out.println("Conta sendo criada. N.: " + this.numero);
-    //}
-    // metodos
-
-    //public int getNumero() {}
-
-  //  public void setNumero(int numero){
-    //    if (numero <= 0){
-      //      System.out.println("Valor não permitido. Inferior a zero.");
-        //    return;
-       // }
-        //this.numero = numero;
-    //}
-
-    //public int getAgencia() {System.out.println("teste getAgencia"); return 0;}
-
-    //public void setAgencia(int agencia) {
-      //  if (numero <= 0){
-        //    System.out.println("Valor não permitido. Inferior a zero.");
-          //  return;
-       // }
-        //this.agencia = agencia;
-    //}
-
-   // public void setTitular(Cliente titular) { }
-
-    //public Cliente getTitular() { }
 
     public void deposita(double valor) { // public nivel de acesso permitidobom
         this.saldo+=valor;
