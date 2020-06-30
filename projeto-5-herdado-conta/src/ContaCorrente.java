8b0de6a0d5ecbbd1e6395aa0f74f2e76f583e271
r-fantos <renato@renato>
2020-06-30 17:12:56 -0300

    Add @override sacar from SuperClass Conta. Bug detected due to signature of sacar() in SuperClass Conta.

--- projeto-5-herdado-conta/src/ContaCorrente.java
+++ projeto-5-herdado-conta/src/ContaCorrente.java
@@ -3,5 +3,11 @@ public class ContaCorrente extends Conta {
     public ContaCorrente(int agencia, int numero) {
         super(agencia, numero);
     }
+
+    @Override // intentional overwrite
+    public boolean sacar(double valor) {
+        double valorAtualizado = valor + 0.2;
+        return super.sacar(valorAtualizado); // taxa de saque da ContaCorrente
+    }
 }
 
