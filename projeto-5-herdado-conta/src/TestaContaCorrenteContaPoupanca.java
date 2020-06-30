397e5afc4de3d1c5c806f4b37e1c4a6fd3f7e377
r-fantos <renato@renato>
2020-06-30 17:15:48 -0300

    TestClass to verify the implementation of constructors in ContaCorrente and ContaPoupanca

--- projeto-5-herdado-conta/src/TestaContaCorrenteContaPoupanca.java
+++ projeto-5-herdado-conta/src/TestaContaCorrenteContaPoupanca.java
@@ -2,5 +2,18 @@ public class TestaContaCorrenteContaPoupanca {
 
     public static void main(String[] arg) {
         ContaCorrente contaCorrenteDoRenato = new ContaCorrente(1234, 5678);
+        ContaPoupanca contaPoupancaDoRenato = new ContaPoupanca(9123, 4567);
+
+        contaCorrenteDoRenato.depositar(100.00);
+        contaPoupancaDoRenato.depositar(100.00);
+
+//        contaCorrenteDoRenato.transferir(50.00, contaPoupancaDoRenato);
+
+        System.out.println("Saldo contaCorrenteDoRenato " + contaCorrenteDoRenato.getSaldo());
+        System.out.println("Saldo contaPoupancaDoRenato " + contaPoupancaDoRenato.getSaldo());
+
+//         teste @override - devo sacar 10, e observa um desconto adicional de 0,2 centavos
+//        contaCorrenteDoRenato.sacar(10.00); // nao funcionou, corrigir a assinatura do metodo
+//        System.out.println("Saldo contaCorrenteDoRenato " + contaCorrenteDoRenato.getSaldo());
     }
 }
