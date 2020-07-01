beb7acb5c403db86d320202941c2da77493ccb47
r-fsantos <renato@renato>
2020-07-01 16:09:22 -0300

    Copy from previous project to work on them in this project

---
+++ projeto-5-herdado-conta/src/Funcionario.java
@@ -0,0 +1,44 @@
+public class Funcionario {
+    public String nome;
+    public String cpf;
+/*
+    protected double salario; // alteracao devido a classe filha referir-se
+    a metodo da classe mae atraves de super.getBonificacao() e super.getSalario()
+    assim nao eh necessario o protected... private resolve! pois valores de
+    atributos sao requisitados via getBonificacao() e getSalario()
+ */
+    private double salario;
+
+    public Funcionario() {
+//        Funcionarios.totalDeFuncionarios ++; // so eh possivel alterar diretamente da classe
+    }
+
+    public double getBonificacao() {
+        System.out.println("Obtendo bonificação para Funcionário padrão");
+        return this.salario * 0.05;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
---
+++ projeto-5-herdado-conta/src/Gerente.java
@@ -0,0 +1,24 @@
+// gerente originida a partir da heranca de Funcionarios
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public boolean autentica(int senha) {
+        return this.senha == senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public double getBonificacao() {
+//        return this.salario;
+//        trocar this por super -> atributo referente a superclasse Funcionario
+//        return super.salario;
+//        return this.salario * 0.1;
+//        return (super.salario * 0.1) + super.salario;
+//        return super.salario * 1.1;
+//        mantendo a assinatura
+        System.out.println("Obtendo bonificação padrão para Gerente");
+        return super.getBonificacao() + super.getSalario();
+    }
+}
