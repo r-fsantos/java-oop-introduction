a4b08ffb2c9aa4208391967ed7caa02164c8fb13
r-fsantos <felicio95.jr@gmail.com>
2020-07-03 16:28:44 -0300

    Change class allowed to acess. From Gerente to FuncionarioAutenticavel

--- projeto-6-herdado/src/SistemaInterno.java
+++ projeto-6-herdado/src/SistemaInterno.java
@@ -2,7 +2,8 @@ public class SistemaInterno {
 
     private int senha = 1234;
 
-    public void autenticar(Gerente g) {
-        System.out.println(g.autenticar(this.senha) ? "Pode entrar no sistema!" : "Não pode entrar no sistema!");
+    public void autenticar(FuncionarioAutenticavel funcionarioAutenticavel) {
+        System.out.println(funcionarioAutenticavel.autenticar(this.senha) ? "Pode entrar no sistema!" : "Não pode entrar no sistema!");
     }
 }
+
