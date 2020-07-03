c4457aab38457f3501c51421aa6316c530eb02b2
r-fsantos <felicio95.jr@gmail.com>
2020-07-03 16:20:09 -0300

    Pull off some unnecessary comments

--- projeto-6-herdado/src/Gerente.java
+++ projeto-6-herdado/src/Gerente.java
@@ -1,13 +1,6 @@
 // gerente originida a partir da heranca de Funcionarios
 public class Gerente extends FuncionarioAutenticavel {
     public double getBonificacao() {
-//        return this.salario;
-//        trocar this por super -> atributo referente a superclasse Funcionario
-//        return super.salario;
-//        return this.salario * 0.1;
-//        return (super.salario * 0.1) + super.salario;
-//        return super.salario * 1.1;
-//        mantendo a assinatura
         System.out.println("Obtendo bonificação padrão para Gerente");
         return super.getSalario();
     }
