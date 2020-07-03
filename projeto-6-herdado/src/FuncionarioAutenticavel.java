6d8cd1e3747d27a939717e69712b597590c005d5
r-fsantos <felicio95.jr@gmail.com>
2020-07-03 16:14:35 -0300

    Add attributes and methods of this class

--- projeto-6-herdado/src/FuncionarioAutenticavel.java
+++ projeto-6-herdado/src/FuncionarioAutenticavel.java
@@ -1,2 +1,12 @@
 public class FuncionarioAutenticavel extends Funcionario{
+    private int senha;
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean autenticar(int senha) {
+        return this.senha == senha;
+    }
+
 }
