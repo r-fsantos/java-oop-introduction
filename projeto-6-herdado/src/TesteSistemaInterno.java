53c418d62ebe1bb272a3e5b280f880dd3da50eaf
r-fsantos <felicio95.jr@gmail.com>
2020-07-03 16:33:39 -0300

    Test new superClass Gerente/Adminitrador extended by FuncionarioAutenticavel

--- projeto-6-herdado/src/TesteSistemaInterno.java
+++ projeto-6-herdado/src/TesteSistemaInterno.java
@@ -3,8 +3,11 @@ public class TesteSistemaInterno {
     public static void main(String[] args) {
         Gerente g = new Gerente();
         g.setSenha(1234);
+        Administrador adm = new Administrador();
+        adm.setSenha(3333);
 
         SistemaInterno siTeste = new SistemaInterno();
         siTeste.autenticar(g);
+        siTeste.autenticar(adm);
     }
 }
