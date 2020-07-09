00c28521b49d74fdf224ee001df3fd4a905fa4fc
r-fsantos <felicio95.jr@gmail.com>
2020-07-09 17:26:39 -0300

    Refactoring FuncionarioAutenticavel.java -> Autenticavel.java

--- projeto-6-herdado/src/Autenticavel.java
+++ projeto-6-herdado/src/Autenticavel.java
@@ -1,4 +1,4 @@
-public abstract class FuncionarioAutenticavel extends Funcionario{
+public abstract class Autenticavel extends Funcionario{
     private int senha;
 
     public void setSenha(int senha) {
