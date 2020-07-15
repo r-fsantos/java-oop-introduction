f8aac1d6ffaf201647d313e4734d1d80f6e21a0b
r-fsantos <felicio95.jr@gmail.com>
2020-07-15 16:00:50 -0300

    setting setSenha as abstract, because the class is abstract

--- projeto-6-herdado/src/Autenticavel.java
+++ projeto-6-herdado/src/Autenticavel.java
@@ -1,9 +1,7 @@
 public abstract class Autenticavel {
     private int senha;
 
-    public void setSenha(int senha) {
-        this.senha = senha;
-    }
+    public abstract void setSenha(int senha);// { this.senha = senha; }
 
     public boolean autenticar(int senha) {
         return this.senha == senha;
