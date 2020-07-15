478b0c9e5d6c52e68f60c4a0df6ba777dc3bed72
r-fsantos <felicio95.jr@gmail.com>
2020-07-15 17:16:15 -0300

    Testing the implementation of Autenticavel

--- projeto-6-herdado/src/TesteSistemaInterno.java
+++ projeto-6-herdado/src/TesteSistemaInterno.java
@@ -5,9 +5,14 @@ public class TesteSistemaInterno {
         g.setSenha(1234);
         Administrador adm = new Administrador();
         adm.setSenha(3333);
+        Cliente cliente = new Cliente();
+        cliente.setSenha(1234);
 
-//        SistemaInterno siTeste = new SistemaInterno();
-//        siTeste.autenticar(g);
-//        siTeste.autenticar(adm);
+        SistemaInterno siTeste = new SistemaInterno();
+        siTeste.autenticar(g);
+        siTeste.autenticar(adm);
+        siTeste.autenticar(cliente);
+
+
     }
 }
