fe25a17e3e2c0114e90d82f2552f0c6bac6f99d4
r-fsantos <felicio95.jr@gmail.com>
2020-07-15 16:37:23 -0300

    Implementing methods of the interface Autenticavel

--- projeto-6-herdado/src/Gerente.java
+++ projeto-6-herdado/src/Gerente.java
@@ -1,7 +1,20 @@
 // gerente originida a partir da heranca de Funcionarios
-public class Gerente extends Funcionario {
+public class Gerente extends Funcionario implements Autenticavel{
+
+    private int senha;
+
     public double getBonificacao() {
         System.out.println("Obtendo bonificação padrão para Gerente");
         return super.getSalario();
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autenticar(int senha) {
+        return this.senha == senha;
     }
 }
