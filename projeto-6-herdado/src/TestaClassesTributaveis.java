8f87c2989f49dd3440bbbfb5333e9b8f3fd9ca98
r-fsantos <felicio95.jr@gmail.com>
2020-07-21 16:23:15 -0300

    TestClass for the use of Interfaces

--- projeto-6-herdado/src/TestaClassesTributaveis.java
+++ projeto-6-herdado/src/TestaClassesTributaveis.java
@@ -1,2 +1,14 @@
 public class TestaClassesTributaveis {
+    public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente(222, 333);
+
+        SeguroDeVida seguro = new SeguroDeVida();
+
+        CalculadorImposto calc = new CalculadorImposto();
+        calc.registrarImposto(cc);
+        calc.registrarImposto(seguro);
+
+        System.out.println(calc.getValorDosImpostos());
+    }
 }
