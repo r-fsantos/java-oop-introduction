9c4f699bf6d1856c84070c4cf25f0f4a1a35efaf
r-fsantos <felicio95.jr@gmail.com>
2020-07-21 15:53:09 -0300

    add methods to calculate and getValorDoImposto

--- projeto-6-herdado/src/CalculadorImposto.java
+++ projeto-6-herdado/src/CalculadorImposto.java
@@ -1,2 +1,12 @@
-public class CalculadorImposto {
+public class CalculadorImposto implements Tributavel{
+
+    private double valorDosImpostos;
+
+    public void registrarImposto(Tributavel t) {
+        this.valorDosImpostos += t.getValorImposto();
+    }
+
+    public double getValorDosImpostos() {
+        return valorDosImpostos;
+    }
 }
